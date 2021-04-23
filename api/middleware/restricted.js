113ccf1899a1684f24f922af402215489edeb716
aleksandr-ardanov <baraboo312@gmail.com>
2021-04-23 12:28:21 -0500

    token done

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,7 +1,21 @@
 const Users = require('../auth/users-model')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../secrets/index')
 
 const restrict = (req, res, next) => {
-  next();
+  const token = req.headers.authorization
+  if(!token){
+    res.status(401).json({message:"token required"})
+  } else{
+    jwt.verify(token,JWT_SECRET,(err,decoded) => {
+      if (err){
+        res.status(401).json({message:"Token invalid"})
+      } else{
+        req.decodedToken = decoded;
+        next()
+      }
+    })
+    }
   /*
     IMPLEMENT
 
